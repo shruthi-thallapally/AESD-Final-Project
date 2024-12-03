b675f88bd7086d6ae619ea0d8374b727d9a47c78
Shruthi Thallapally <shruthi.thallapally@colorado.edu>
2024-12-02 23:20:54 -0700

    sensor working

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'affb8f4cee6578bf57036d84c4a06d4e296d83e9'
+AESD_ASSIGNMENTS_VERSION = 'eb006868dcd2c8a0d9581c4484def5c0504c6d55'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -16,13 +16,15 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 #	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/sht21 all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	#$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	#$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/sht21/sht21.py $(TARGET_DIR)/usr/bin
+	#$(INSTALL) -m 0755 $(@D)/sht21/sht21.py $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/sht21/* $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
